511f199d13e47d4179bcfaa806ea97d551107b31
Artur Kasprzyk <a.kasprzyk@merixstudio.com>
2017-08-10 13:44:48 +0200

    Remove redux, react router, material design. Add jest.

--- app/index.jsx
+++ app/index.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  Provider
-} from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import configureStore from './store';
 
 injectTapEventPlugin();
-
-const store = configureStore();
 
 const rootElement = document.getElementById('app');
 
 const app = (
-  <Provider store={store}>
-    <MuiThemeProvider>
-      <Router>
-        <div>
-          <p>Bitstatistics</p>
-        </div>
-      </Router>
-    </MuiThemeProvider>
-  </Provider>
+  <div>
+    <p>Bitstatistics</p>
+  </div>
 );
 
 ReactDOM.render(
