c67be619e36632f0a28a2a28124f632cf7dbeac4
Artur Kasprzyk <a.kasprzyk@merixstudio.com>
2017-08-10 15:56:33 +0200

    Add chart with temporary data

--- app/components/Chart/index.jsx
+++ app/components/Chart/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import {
+  HorizontalBar,
+  defaults,
+} from 'react-chartjs-2';
 
 import './Chart.scss';
 
 export default class Chart extends Component {
   constructor(props) {
     super(props);
+
+    defaults.global.legend = false;
   }
 
   render() {
@@ -13,6 +18,9 @@ export default class Chart extends Component {
       <div className="chart">
         <h2 className="chart__title">{this.props.title}</h2>
         <p className="chart__description">{this.props.description}</p>
+        <HorizontalBar
+          data={this.props.data}
+        />
       </div>
     );
   }
--- app/containers/Statistics/index.jsx
+++ app/containers/Statistics/index.jsx
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 import { Chart } from '../../components';
 
 import './Statistics.scss';
 
 export default class Statistics extends Component {
+  constructor() {
+    super();
+
+    this.repositories = {
+      labels: [moment().format('DD/MM/YYYY'), moment().day(-1).format('DD/MM/YYYY')],
+      datasets: [
+        {
+          backgroundColor: 'rgb(222,143,51)',
+          data: [44, 65],
+        },
+      ],
+    };
+  }
+
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div className="statistics">
         <Chart
           title="Repositories"
           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+          data={this.repositories}
         />
       </div>
     );
