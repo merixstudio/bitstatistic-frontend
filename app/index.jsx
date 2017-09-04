42e1c1e0e618dae0304f347a56447ecdd6e2fd55
Artur Kasprzyk <a.kasprzyk@merixstudio.com>
2017-09-04 12:43:04 +0200

    Add react native basic app.

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,6 @@ import './styles/main.scss';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import {
   Header,
@@ -14,8 +13,6 @@ import {
 import {
   Statistics,
 } from './containers';
-
-injectTapEventPlugin();
 
 const rootElement = document.getElementById('app');
 
