6de43ea4151dd206a0236c93aacfc094cb02e15f
Artur Kasprzyk <a.kasprzyk@merixstudio.com>
2017-09-05 11:43:27 +0200

    Add header

--- app/native/components/Header/index.js
+++ app/native/components/Header/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {
   View,
   Image,
+  Text,
 } from 'react-native';
 
 import { styles } from './style';
@@ -10,11 +11,29 @@ import { styles } from './style';
 export default class Header extends Component {
   render() {
     return (
-      <View
-        style={styles.header}
-      >
-        <Image
-          source={require('../../../assets/images/merix_logo_text.png')}
+      <View>
+        <View
+          style={styles.header}
+        >
+          <View
+            style={styles.content}
+          >
+            <Image
+              style={styles.image}
+              source={require('../../../assets/images/merix_logo_text.png')}
+            />
+            <Text
+              style={styles.title}
+            >
+              Bitstatistics
+            </Text>
+          </View>
+          <Image
+            source={require('../../../assets/images/header_arrows.png')}
+          />
+        </View>
+        <View
+          style={styles.border}
         />
       </View>
     );
--- app/native/components/Header/style.js
+++ app/native/components/Header/style.js
@@ -2,6 +2,27 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   header: {
-
+    height: 56,
+    backgroundColor: '#f7f7f7',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  content: {
+    padding: 10,
+    flexDirection: 'row',
+  },
+  image: {
+    width: 120,
+    height: 37,
+  },
+  title: {
+    fontSize: 20,
+    color: '#de8f33',
+    paddingLeft: 20,
+    paddingTop: 5,
+  },
+  border: {
+    height: 1,
+    backgroundColor: '#d7d7d7',
   },
 });
